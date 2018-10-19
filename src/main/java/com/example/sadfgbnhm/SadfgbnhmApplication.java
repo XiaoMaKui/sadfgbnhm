de9ef5594f1fa212459e156894ca933b4f1000a9
XiaoMaKui <1543350760@qq.com>
2018-10-19 11:24:44 +0800

    Initial commit

---
+++ src/main/java/com/example/sadfgbnhm/SadfgbnhmApplication.java
@@ -0,0 +1,12 @@
+package com.example.sadfgbnhm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SadfgbnhmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SadfgbnhmApplication.class, args);
+    }
+}
